212407eb90be4af99840a4f8c69a669c7acd812d
Lukas Polak <lpolak@dioscouri.com>
2014-07-17 11:59:39 -0400

    Added auto-loading assets from library

--- bootstrap.php
+++ bootstrap.php
@@ -3,5 +3,20 @@ class MinifyBootstrap extends \Dsc\Bootstrap
 {
     protected $dir = __DIR__;
     protected $namespace = 'Minify';
+    
+
+    /**
+     * This part is common for all running all parts of application (both Admin and Site)
+     *
+     * @param $app Name
+     *            of the part of application
+     */
+    protected function runBase($app)
+    {
+    	\Minify\Factory::registerPath(\Base::instance()->get('PATH_ROOT') . 'vendor/dioscouri/f3-lib/src/Dsc/Assets' );
+    	 
+    	parent::runBase( $app );
+    }
+    
 }
 $app = new MinifyBootstrap();
