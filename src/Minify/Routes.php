2dd1714294f01b3bab6ba5e2b5255f6954e4ad55
Rafael Diaz-Tushman <rdiaztushman@dioscouri.com>
2014-08-25 17:33:43 -0400

    disable route caching

--- src/Minify/Routes.php
+++ src/Minify/Routes.php
@@ -6,6 +6,14 @@ class Routes extends \Dsc\Routes\Group
 
     public function initialize()
     {
+        $this->app->route('GET /minify/css', '\Minify\Controller->css');
+        $this->app->route('GET /minify/js', '\Minify\Controller->js');
+        $this->app->route('GET /minify/*', '\Minify\Controller->item');
+        $this->app->route('GET /admin/minify/css', '\Minify\Controller->css');
+        $this->app->route('GET /admin/minify/js', '\Minify\Controller->js');
+        $this->app->route('GET /admin/minify/*', '\Minify\Controller->item');
+                
+        /*
         if ($this->app->get('DEBUG') || $this->input->get('refresh', 0, 'int')) 
         {
             $this->app->route('GET /minify/css', '\Minify\Controller->css');
@@ -27,5 +35,6 @@ class Routes extends \Dsc\Routes\Group
             $this->app->route('GET /admin/minify/js', '\Minify\Controller->js');
             $this->app->route('GET /admin/minify/*', '\Minify\Controller->item');
         }
+        */
     }
 }
